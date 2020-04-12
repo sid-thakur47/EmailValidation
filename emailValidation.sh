17a55a105f563b4ada57884b07fcfa1053be4de7
sid-thakur47 <sidthakur258@gmail.com>
2020-04-12 12:12:14 +0530

    ADDED:Regex of optional part of email

--- emailValidation.sh
+++ emailValidation.sh
@@ -3,8 +3,7 @@
 shopt -s extglob
 
 read -p "Enter email id:" emailId
-#allowed characters
-emailPattern="^[A-Za-z0-9]{4,}([+|_|-])?[A-Za-z]{4,}@[a-z]{2,5}.[a-z]{1,3}$"
+emailPattern="^[A-Za-z0-9]{4,}([+|_|-])?[A-Za-z0-9]{4,}@[a-z]{2,5}.[a-z]{1,3}.([a-z]{1,3})?$"
 	if [[ $emailId =~ $emailPattern ]]
 	then
 		echo "Valid email"
